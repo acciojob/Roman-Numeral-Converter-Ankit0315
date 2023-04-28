3a6c65e7aa33d4d5f21aa5ad7fd4d6b3b1f05abd
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-28 11:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ function convertToRoman(num) {
   if(num < 1)
   { return "";}
   if(num >= 40)
-  { return "XL" + convert(num - 40);}
+  { return "XL" + convertToRoman(num - 40);}
 			
   if(num >= 10)
-  { return "X" + convert(num - 10);}
+  { return "X" + convertToRoman(num - 10);}
   if(num >= 9)
-  { return "IX" + convert(num - 9);}
+  { return "IX" + convertToRoman(num - 9);}
   if(num >= 5)
-  { return "V" + convert(num - 5);}
+  { return "V" + convertToRoman(num - 5);}
   if(num >= 4)
-  { return "IV" + convert(num - 4);}
+  { return "IV" + convertToRoman(num - 4);}
   if(num >= 1)
-  { return "I" + convert(num - 1);}  
+  { return "I" + convertToRoman(num - 1);}  
 
 
 }
