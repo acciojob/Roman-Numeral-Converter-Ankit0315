a320f0dee3ca13379e647fad9729624d8f67b542
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-28 11:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,29 @@ function convertToRoman(num) {
     };
 
   //your code here
+	function convert(num)
+		{ 
+  if(num < 1)
+  { return "";}
+  if(num >= 40)
+  { return "XL" + convert(num - 40);}
+			
+  if(num >= 10)
+  { return "X" + convert(num - 10);}
+  if(num >= 9)
+  { return "IX" + convert(num - 9);}
+  if(num >= 5)
+  { return "V" + convert(num - 5);}
+  if(num >= 4)
+  { return "IV" + convert(num - 4);}
+  if(num >= 1)
+  { return "I" + convert(num - 1);}  
+}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
